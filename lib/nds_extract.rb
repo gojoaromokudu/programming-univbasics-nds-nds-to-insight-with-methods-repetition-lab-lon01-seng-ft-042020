e70182e28243b1b7c170f1a36cc42ab936d8efde
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-03-02 17:38:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,8 +48,6 @@ def total_gross(source)
   # return the total
   
   big_total = 0 
-#  puts list_of_directors(source)[0]
-# puts directors_totals(source)["Stephen Spielberg"]
   director_array_index = 0
   while director_array_index < list_of_directors(source).length do
     puts directors_totals(source)[list_of_directors(source)[director_array_index]]
